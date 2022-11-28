591c276a3a2c64c22a3b2a2003c1dab4824258ed
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-11-28 12:34:46 -0500

    Add apple

--- MyWorld.java
+++ MyWorld.java
@@ -7,6 +7,11 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Elephant e = new Elephant();
-        addObject(e, 100, 100);
+        addObject(e, 100, 300);
+        
+        Apple apple = new Apple();
+        int x = getWidth()/2;
+        int y = 10;
+        addObject(apple, x, y);
     }
 }
