82e7b7aa64b09e3b90147f5fb889e2ccea211d6d
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-12-07 12:32:10 -0500

    Add animation

--- Elephant.java
+++ Elephant.java
@@ -3,9 +3,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound sound;
+    SimpleTimer animTimer = new SimpleTimer();
+    GreenfootImage[] images = new GreenfootImage[8];
     
     public Elephant(){
         sound = new GreenfootSound("sounds/elephantcub.mp3");
+        
+        for(int i = 0; i < images.length; i++){
+            images[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            images[i].scale(100, 100);
+        }
+        
+        setImage(images[4]);
+        
+        animTimer.mark();
+    }
+    
+    int i = 0;
+    public void animate(){
+        if (animTimer.millisElapsed() > 100){
+            setImage(images[i]);
+            i = (i + 1) % images.length;
+            animTimer.mark();
+        }
     }
     
     public void act()
@@ -18,6 +38,7 @@ public class Elephant extends Actor
         }
         
         eat();
+        animate();
     }
     
     public void eat(){
